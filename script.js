9a0320f01455eab555b56ca02011753794db0e73
Jr <jubayerrahman203@gmail.com>
2023-08-04 00:27:57 +0600

    fd

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-// write your JavaScript here
+const nav__items = document.getElementsByClassName("nav__items");
+const hamburgerActive = (hamburger) => {
+  hamburger.classList.toggle("active");
+  nav__items[0].classList.toggle("active");
+  // console.log(nav__items[0]);
+};
